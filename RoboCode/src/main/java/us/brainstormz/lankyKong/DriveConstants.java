7a76729f87a7d15990b974f5f693048e7924d347
AlephBibleo <gabefergesen@gmail.com>
2022-11-05 22:02:29 -0700

    Gabe: Normalizes PID errors.

--- RoboCode/src/main/java/us/brainstormz/lankyKong/DriveConstants.java
+++ RoboCode/src/main/java/us/brainstormz/lankyKong/DriveConstants.java
@@ -87,8 +87,8 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      
      */
-    public static double MAX_VEL = 52.48291908330528;
-    public static double MAX_ACCEL = 52.48291908330528;
+    public static double MAX_VEL = 40;
+    public static double MAX_ACCEL = 40;
     public static double MAX_ANG_VEL = Math.toRadians(214.78926857142858);
     public static double MAX_ANG_ACCEL = Math.toRadians(214.78926857142858);
 
