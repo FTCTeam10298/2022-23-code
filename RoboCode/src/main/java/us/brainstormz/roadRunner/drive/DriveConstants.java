14790c9e2ea31e79cfc2371b0e133a3c51c4e590
AlephBibleo <gabefergesen@gmail.com>
2022-12-02 21:29:55 -0800

    Gabe: Basic Feedforward... tunedsay the vault door has sealed

--- RoboCode/src/main/java/us/brainstormz/roadRunner/drive/DriveConstants.java
+++ RoboCode/src/main/java/us/brainstormz/roadRunner/drive/DriveConstants.java
@@ -54,8 +54,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.387 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
+    public static double kV = 0.387;
+    public static double kA = .02;
     public static double kStatic = 0;
 
     /*
