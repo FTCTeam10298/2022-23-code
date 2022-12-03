61a8ebb734d113b05fe55a373c31675ccaab5cbb
AlephBibleo <gabefergesen@gmail.com>
2022-12-02 21:07:48 -0800

    Gabe: Retards RR Vals.

--- RoboCode/src/main/java/us/brainstormz/roadRunner/drive/DriveConstants.java
+++ RoboCode/src/main/java/us/brainstormz/roadRunner/drive/DriveConstants.java
@@ -1,3 +1,4 @@
+//Winnie Config
 package us.brainstormz.roadRunner.drive;
 
 import com.acmerobotics.dashboard.config.Config;
@@ -31,7 +32,7 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = true;
+    public static final boolean RUN_USING_ENCODER = false; // Jargon. Toggles REV internal PID (inferior)
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
             getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
 
@@ -53,7 +54,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.378 / rpmToVelocity(MAX_RPM);
     public static double kA = 0;
     public static double kStatic = 0;
 
@@ -64,8 +65,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
-    public static double MAX_ACCEL = 30;
+    public static double MAX_VEL = .9;
+    public static double MAX_ACCEL = 7.5;
     public static double MAX_ANG_VEL = Math.toRadians(180);
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
