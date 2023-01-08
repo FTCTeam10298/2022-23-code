33056fa2104c38cafed191612e729217528ba317
QwertyChouskie <asdfghrbljzmkd@outlook.com>
2023-01-07 16:53:52 -0800

    fixed rrlocalizer

--- RoboCode/src/main/java/us/brainstormz/motion/RRLocalizer.kt
+++ RoboCode/src/main/java/us/brainstormz/motion/RRLocalizer.kt
@@ -4,7 +4,9 @@ import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior
 import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.Telemetry
 import us.brainstormz.hardwareClasses.MecanumDriveTrain
 import us.brainstormz.hardwareClasses.MecanumHardware
 import us.brainstormz.hardwareClasses.ThreeWheelOdometry
@@ -35,19 +37,33 @@ class RRLocalizer(hardware: ThreeWheelOdometry): Localizer {
     }
 }
 
-@Autonomous
-class RRLocalizerTest: OpMode() {
-    val hardware = PaddieMatrickHardware()
+class Foo(private val hardware: PaddieMatrickHardware, private val telemetry:Telemetry){
     val localizer = RRLocalizer(hardware)
 
-    override fun init() {
-        hardware.init(hardwareMap)
-    }
-
-    override fun loop() {
+    fun loop(){
         localizer.recalculatePositionAndRotation()
         telemetry.addLine("current pos: ${localizer.currentPositionAndRotation()}")
         telemetry.update()
+    }
+}
+
+@Autonomous
+class RRLocalizerTest: OpMode() {
+    var foo:Foo? = null
+    val hardware = PaddieMatrickHardware()
+    val movement = MecanumDriveTrain(hardware)
+
+    override fun init() {
+        hardware.init(hardwareMap)
+        hardware.lFDrive.zeroPowerBehavior = ZeroPowerBehavior.FLOAT
+        hardware.rFDrive.zeroPowerBehavior =  ZeroPowerBehavior.FLOAT
+        hardware.lBDrive.zeroPowerBehavior =  ZeroPowerBehavior.FLOAT
+        hardware.rBDrive.zeroPowerBehavior =  ZeroPowerBehavior.FLOAT
+        foo = Foo(hardware, telemetry)
+    }
+
+    override fun loop() {
+        foo?.loop()
     }
 
 }
