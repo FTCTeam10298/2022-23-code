189f7e50cf2b392bf54b1e282de0cb70ed27de5f
stheyounger <sj2@penrose.us>
2023-02-08 17:10:35 -0800

    deleted rr artifacts

--- RoboCode/src/main/java/us/brainstormz/paddieMatrick/PaddieMatrickHardware.kt
+++ RoboCode/src/main/java/us/brainstormz/paddieMatrick/PaddieMatrickHardware.kt
@@ -1,6 +1,5 @@
 package us.brainstormz.paddieMatrick
 
-import com.qualcomm.hardware.bosch.BNO055IMU
 import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.hardware.rev.RevColorSensorV3
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot
@@ -12,9 +11,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.VoltageUnit
 import us.brainstormz.hardwareClasses.EnhancedDCMotor
 import us.brainstormz.hardwareClasses.MecanumHardware
 import us.brainstormz.hardwareClasses.ThreeWheelOdometry
-
-private const val s = "rEncoder"
-private const val freeMove = false //for debugging. puts motors in float when stopped.
 
 class PaddieMatrickHardware: MecanumHardware, ThreeWheelOdometry {
     override lateinit var lFDrive: DcMotor
