fabc41928ec6f833eabf4c470ad200e963b6ba63
stheyounger <sj2@penrose.us>
2023-02-23 07:48:59 -0800

    created basic junction autoaim test

---
+++ RoboCode/src/main/java/us/brainstormz/paddieMatrick/JunctionAimbotTest.kt
@@ -0,0 +1,73 @@
+package us.brainstormz.paddieMatrick
+
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.openftc.easyopencv.OpenCvCameraRotation
+import us.brainstormz.hardwareClasses.MecOdometry
+import us.brainstormz.localizer.OdometryLocalizer
+import us.brainstormz.localizer.PositionAndRotation
+import us.brainstormz.localizer.polePosition.creamsicleGoalDetection.CreamsicleGoalDetector
+import us.brainstormz.motion.MecanumMovement
+import us.brainstormz.motion.RRLocalizer
+import us.brainstormz.openCvAbstraction.OpenCvAbstraction
+import us.brainstormz.telemetryWizard.TelemetryConsole
+
+@TeleOp
+class JunctionAimbotTest: OpMode() {
+
+    private val hardware = PaddieMatrickHardware()
+
+    private lateinit var movement: MecanumMovement
+
+
+    val console = TelemetryConsole(telemetry)
+    val dashTelemetry = FtcDashboard.getInstance().telemetry
+    val multiTelemetry = MultipleTelemetry(telemetry, dashTelemetry)
+
+    val opencv = OpenCvAbstraction(this)
+    private val junctionDetector = CreamsicleGoalDetector(console)
+
+    override fun init() {
+        hardware.init(hardwareMap)
+        val localizer= RRLocalizer(hardware)
+        movement = MecanumMovement(localizer, hardware, telemetry)
+
+        opencv.cameraName = "liftCam"
+        opencv.cameraOrientation = OpenCvCameraRotation.UPSIDE_DOWN
+        opencv.init(hardwareMap)
+        opencv.start()
+        opencv.onNewFrame(junctionDetector::scoopFrame)
+    }
+
+    var initAngle = 0.0
+    override fun start() {
+        initAngle = movement.localizer.currentPositionAndRotation().r
+    }
+
+    override fun loop() {
+
+        val centeredPosition: Double = 165.0
+        val junctionX = junctionDetector.xPositionOfJunction() ?: centeredPosition
+        val junctionErrorFromCenter = junctionX - centeredPosition
+
+        val turnSpeed = 0.4
+        val targetAngle = initAngle - (junctionErrorFromCenter *turnSpeed).coerceIn(-180.0..180.0)
+        val position = PositionAndRotation(r= targetAngle)
+
+        val robotIsAimedTrue = movement.moveTowardTarget(position, 0.0..0.2)
+
+        multiTelemetry.addLine("junctionX: $junctionX")
+        multiTelemetry.addLine("centeredPosition: $centeredPosition")
+        multiTelemetry.addLine("junctionErrorFromCenter: $junctionErrorFromCenter")
+        multiTelemetry.addLine("\ntargetAngle: $targetAngle")
+        multiTelemetry.addLine("position: $position")
+        multiTelemetry.addLine("\nrobotIsAimedTrue: $robotIsAimedTrue")
+        multiTelemetry.addLine("current time: ${System.currentTimeMillis()}")
+        multiTelemetry.update()
+
+        println("OpMode is running 69420")
+    }
+
+}
