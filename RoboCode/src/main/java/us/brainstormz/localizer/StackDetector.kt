70aa410f6bc920226cd7a663e4602f7dc4d77e8d
stheyounger <sj2@penrose.us>
2023-02-28 22:08:36 -0800

    stack detector and tuner

---
+++ RoboCode/src/main/java/us/brainstormz/localizer/StackDetector.kt
@@ -0,0 +1,182 @@
+package us.brainstormz.localizer
+
+import org.opencv.core.*
+import org.opencv.imgproc.Imgproc
+import kotlin.math.abs
+
+data class StackDetection(
+    val xPosition:Double,
+    val whenDetected:Long
+)
+
+class StackDetectorVars(
+    var targetHue:StackDetector.TargetHue,
+    var displayMode:StackDetector.Mode
+)
+
+class StackDetector(private val vars:StackDetectorVars){
+
+    enum class Mode {
+        FRAME,
+        MASK
+    }
+
+    enum class TargetHue {
+        RED,
+        BLUE,
+        absRED
+    }
+
+    class NamedVar(val name: String, var value: Double)
+
+
+    class ColorRange(val L_H: NamedVar, val L_S: NamedVar, val L_V: NamedVar, val U_H: NamedVar, val U_S: NamedVar, val U_V: NamedVar)
+
+    private val redSolarizedColor = ColorRange(
+            L_H = NamedVar("Low Hue", 120.0),
+            L_S = NamedVar("Low Saturation", 150.0),
+            L_V = NamedVar("Low Vanity/Variance/VolumentricVibacity", 0.0),
+            U_H = NamedVar("Uppper Hue", 130.0),
+            U_S = NamedVar("Upper Saturation", 255.0),
+            U_V = NamedVar("Upper Vanity/Variance/VolumentricVibracity", 200.0)
+    )
+
+    private val redAbsoluteColor = ColorRange(
+            L_H = NamedVar("Low Hue", 120.0),
+            L_S = NamedVar("Low Saturation", 150.0),
+            L_V = NamedVar("Low Vanity/Variance/VolumentricVibacity", 0.0),
+            U_H = NamedVar("Uppper Hue", 255.0),
+            U_S = NamedVar("Upper Saturation", 255.0),
+            U_V = NamedVar("Upper Vanity/Variance/VolumentricVibracity", 255.0)
+    )
+
+    val redColor = ColorRange(
+            L_H = NamedVar("Low Hue", 0.0),
+            L_S = NamedVar("Low Saturation", 0.0),
+            L_V = NamedVar("Low Vanity/Variance/VolumentricVibacity", 0.0),
+            U_H = NamedVar("Uppper Hue", 255.0),
+            U_S = NamedVar("Upper Saturation", 255.0),
+            U_V = NamedVar("Upper Vanity/Variance/VolumentricVibracity", 255.0))
+
+    //trained for yellow
+    val blueColor = ColorRange(
+            L_H = NamedVar("Low Hue", 30.0),
+            L_S = NamedVar("Low Saturation", -200.0),
+            L_V = NamedVar("Low Vanity/Variance/VolumentricVibacity", 85.0),
+            U_H = NamedVar("Upper Hue", 255.0),
+            U_S = NamedVar("Upper Saturation", 255.0),
+            U_V = NamedVar("Upper Vanity/Variance/VolumentricVibracity", 255.0))
+
+    val goalColor: ColorRange = when(vars.targetHue){
+        TargetHue.RED -> redColor
+        TargetHue.BLUE -> blueColor
+        TargetHue.absRED -> redAbsoluteColor
+    }
+
+//    var x = 0.0
+//    var y = 0.0
+
+    private val hsv = Mat()
+    private val maskA = Mat()
+    private val maskB = Mat()
+    private val kernel = Mat(5, 5, CvType.CV_8U)
+
+    private var mostRecentDetection:StackDetection? = null
+
+    fun detectedPosition(staleThresholdAgeMillis:Long):Double? {
+        return mostRecentDetection?.let{detection ->
+            val t = System.currentTimeMillis() - staleThresholdAgeMillis
+            if(detection.whenDetected >= t) detection.xPosition else null
+        }
+    }
+
+    fun convert(matOfPoint2f: MatOfPoint2f): MatOfPoint {
+        val foo = MatOfPoint()
+        matOfPoint2f.convertTo(foo, CvType.CV_32S)
+        return foo
+    }
+
+    private fun heightOfLineOnScreen(line:Pair<Point, Point>):Double {
+        return line.second.y - line.first.y
+
+    }
+
+    fun processFrame(frame: Mat): Mat {
+
+        // Pre-process the image to make contour detection easier
+        Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_BGR2HSV)
+        val lower = Scalar(blueColor.L_H.value, blueColor.L_S.value, blueColor.L_V.value)
+        val upper = Scalar(blueColor.U_H.value, blueColor.U_S.value, blueColor.U_V.value)
+        Core.inRange(hsv, lower, upper, maskA)
+        Imgproc.erode(maskA, maskB, kernel)
+
+        // Find the contours
+        val contours = mutableListOf<MatOfPoint>()
+        Imgproc.findContours(maskB, contours, Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE)
+
+        // Dig through them
+        val shapesByArea = contours.map{ cnt: MatOfPoint ->
+            val area = Imgproc.contourArea(cnt)
+
+            fun convert(src: MatOfPoint): MatOfPoint2f {
+                val dst = MatOfPoint2f()
+                src.convertTo(dst, CvType.CV_32F)
+                return dst
+            }
+
+            val cnt2f = convert(cnt)
+            val points = MatOfPoint2f()
+            Imgproc.approxPolyDP(cnt2f, points, 0.02 * Imgproc.arcLength(cnt2f, true), true)
+//
+//            drawLines(points, frame)
+
+            val pointsList = points.toList()
+
+            val pointWeLike = pointsList.firstOrNull()
+            if(pointWeLike!=null){
+                mostRecentDetection = StackDetection(
+                        xPosition = pointWeLike.x,
+                        whenDetected = System.currentTimeMillis()
+                )
+            }
+            area to pointsList
+        }
+
+        val largestShape = shapesByArea.maxByOrNull { it.first }?.second
+
+        if(largestShape!=null){
+            drawLinesFromPoints(largestShape, frame)
+        }
+
+        return when (vars.displayMode) {
+            Mode.FRAME -> frame
+            Mode.MASK -> maskB
+        }
+    }
+
+    private fun areRoughlyInSameXPlane(a: Point, b: Point): Boolean {
+        val fudge = 42
+        return abs(b.x - a.x) <= fudge
+    }
+
+
+    fun drawLinesFromPoints(points: List<Point>, toFrame:Mat) {
+        Imgproc.drawContours(
+                toFrame,
+                listOf(convert(MatOfPoint2f(*(points.toTypedArray())))),
+                0,
+                Scalar(0.0, 255.0, 0.0),
+                2)
+    }
+
+    fun drawLines(points: MatOfPoint2f, toFrame:Mat) {
+
+        Imgproc.drawContours(
+                toFrame,
+                listOf(convert(points)),
+                0,
+                Scalar(0.0, 255.0, 0.0),
+                2)
+    }
+
+}
---
+++ RoboCode/src/main/java/us/brainstormz/paddieMatrick/StackDetectorTuner.kt
@@ -0,0 +1,117 @@
+package us.brainstormz.paddieMatrick
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import org.openftc.easyopencv.OpenCvCamera
+import org.openftc.easyopencv.OpenCvCameraRotation
+import us.brainstormz.localizer.StackDetector
+import us.brainstormz.localizer.StackDetectorVars
+import us.brainstormz.localizer.polePosition.creamsicleGoalDetection.CreamsicleConfig
+import us.brainstormz.localizer.polePosition.creamsicleGoalDetection.CreamsicleGoalDetector
+import us.brainstormz.openCvAbstraction.OpenCvAbstraction
+import us.brainstormz.telemetryWizard.TelemetryConsole
+
+
+@Autonomous(name = "Stack Detector Tuner", group = "Tests")
+class StackDetectorTuner: OpMode() {
+    data class ControllerStatus(val yPressed: Boolean,
+                                val aPressed: Boolean,
+                                val xPressed: Boolean,
+                                val dpadLeftPressed: Boolean,
+                                val rBumperPressed: Boolean,
+                                val lBumperPressed: Boolean)
+
+    val console = TelemetryConsole(telemetry)
+    val opencv = OpenCvAbstraction(this)
+
+    private val stackDetectorVars = StackDetectorVars(StackDetector.TargetHue.BLUE, StackDetector.Mode.FRAME)
+    private val detector = StackDetector(stackDetectorVars)
+
+    private val cameraNameInMap = "backCam"
+    override fun init() {
+        opencv.cameraName = cameraNameInMap
+        opencv.cameraOrientation = OpenCvCameraRotation.UPRIGHT
+        opencv.init(hardwareMap)
+        opencv.start()
+        opencv.onNewFrame(detector::processFrame)
+    }
+
+    private var previousLoopControllerStatus: ControllerStatus = ControllerStatus(
+            yPressed = false,
+            aPressed = false,
+            xPressed = false,
+            dpadLeftPressed = false,
+            rBumperPressed = false,
+            lBumperPressed = false)
+    private var varBeingEdited: StackDetector.NamedVar = detector.goalColor.L_H
+    override fun init_loop() {
+        telemetry.addLine( "Detected Position: ${detector.detectedPosition(1000)}")
+
+        if (gamepad1.x && !previousLoopControllerStatus.xPressed) {
+            telemetry.addLine("TrainerMODE; ${stackDetectorVars.displayMode}")
+            when (stackDetectorVars.displayMode) {
+                StackDetector.Mode.FRAME -> stackDetectorVars.displayMode = StackDetector.Mode.MASK
+                StackDetector.Mode.MASK -> stackDetectorVars.displayMode = StackDetector.Mode.FRAME
+            }
+        }
+
+        telemetry.addLine("varBeingEdited: \n   name ${varBeingEdited.name}\n   value ${varBeingEdited.value}")
+
+        telemetry.addLine("All values: \nL_S ${detector.goalColor.L_S.value}\nL_H ${detector.goalColor.L_H.value}\nL_V ${detector.goalColor.L_V.value}\nU_H ${detector.goalColor.U_H.value}\nU_S ${detector.goalColor.U_S.value}\n U_V${detector.goalColor.U_V.value}")
+        if (gamepad1.dpad_left && !previousLoopControllerStatus.dpadLeftPressed) {
+            varBeingEdited = when (varBeingEdited) {
+                detector.goalColor.L_H -> detector.goalColor.L_S
+                detector.goalColor.L_S -> detector.goalColor.L_V
+                detector.goalColor.L_V -> detector.goalColor.U_H
+                detector.goalColor.U_H -> detector.goalColor.U_S
+                detector.goalColor.U_S -> detector.goalColor.U_V
+                detector.goalColor.U_V -> detector.goalColor.L_H
+                else-> detector.goalColor.L_H
+            }
+        }
+
+        if (gamepad1.y && !previousLoopControllerStatus.yPressed) {
+            telemetry.addLine( "Vals Zeroed")
+            console.queueToTelemetry()
+            detector.goalColor.L_S.value = 0.0
+            detector.goalColor.L_H.value = 0.0
+            detector.goalColor.L_V.value = 0.0
+            detector.goalColor.U_H.value = 0.0
+            detector.goalColor.U_S.value = 0.0
+            detector.goalColor.U_V.value = 0.0
+        }
+
+        if (gamepad1.right_bumper && !previousLoopControllerStatus.rBumperPressed) {
+            varBeingEdited.value += 5
+        }
+
+        if (gamepad1.left_bumper && !previousLoopControllerStatus.lBumperPressed) {
+            varBeingEdited.value -= 5
+        }
+
+        if (gamepad1.a && !previousLoopControllerStatus.aPressed) {
+            telemetry.addLine("Vals set to max range")
+            detector.goalColor.L_H.value = 0.0
+            detector.goalColor.L_S.value = 0.0
+            detector.goalColor.L_V.value = 0.0
+            detector.goalColor.U_H.value = 255.0
+            detector.goalColor.U_S.value = 255.0
+            detector.goalColor.U_V.value = 255.0
+            print("Reality: ${detector.goalColor.L_H.value}, ${detector.goalColor.L_S.value},  ${detector.goalColor.L_V.value},  ${detector.goalColor.U_H.value}, ${detector.goalColor.U_S.value}, ${detector.goalColor.U_V.value}")
+        }
+
+        telemetry.update()
+        previousLoopControllerStatus = ControllerStatus(
+                yPressed = gamepad1.y,
+                aPressed = gamepad1.a,
+                xPressed = gamepad1.x,
+                dpadLeftPressed = gamepad1.dpad_left,
+                rBumperPressed = gamepad1.right_bumper,
+                lBumperPressed = gamepad1.left_bumper)
+    }
+
+    override fun loop() {
+        telemetry.addLine("Only works during init so that we can see the camera output.")
+        telemetry.update()
+    }
+}
