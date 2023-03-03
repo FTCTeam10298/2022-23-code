97361934ac2806ede1c86c01b7dacc88c5be5461
stheyounger <sj2@penrose.us>
2023-03-03 13:34:55 -0800

    mounted camera upside down

--- RoboCode/src/main/java/us/brainstormz/paddieMatrick/PaddieMatrickAuto.kt
+++ RoboCode/src/main/java/us/brainstormz/paddieMatrick/PaddieMatrickAuto.kt
@@ -635,7 +635,7 @@ class PaddieMatrickAuto: OpMode() {
         backPipeline.userFun = stackDetector::processFrame
         backCam = dualCamAbstraction.startNewCamera(
                 cameraName = backCamName,
-                cameraRotation = OpenCvCameraRotation.UPRIGHT,
+                cameraRotation = OpenCvCameraRotation.UPSIDE_DOWN,
                 viewportContainerId = viewportContainerIds[1],
                 pipeline = backPipeline)
     }
