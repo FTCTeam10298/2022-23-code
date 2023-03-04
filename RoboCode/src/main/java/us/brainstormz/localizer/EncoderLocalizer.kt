924052ed54dd51ea3a9750e65f20c9c746096aa6
stheyounger <sj2@penrose.us>
2023-03-03 16:34:40 -0800

    removed todos from photelemetry

--- RoboCode/src/main/java/us/brainstormz/localizer/EncoderLocalizer.kt
+++ RoboCode/src/main/java/us/brainstormz/localizer/EncoderLocalizer.kt
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.hardware.*
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType
 import org.firstinspires.ftc.robotcore.external.Func
 import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.robotcore.external.Telemetry.Item
+import org.firstinspires.ftc.robotcore.external.Telemetry.Log
 import us.brainstormz.hardwareClasses.EnhancedDCMotor
 import us.brainstormz.hardwareClasses.MecOdometry
 import us.brainstormz.hardwareClasses.MecanumHardware
@@ -115,109 +117,118 @@ class PhoHardware(): MecOdometry {
     val telemetry = PhoTelemetry()
 
     class PhoTelemetry(): Telemetry {
-        override fun addData(caption: String?, format: String?, vararg args: Any?): Telemetry.Item {
-            TODO("Not yet implemented")
+        private val phoItem = object: Item {
+            override fun getCaption(): String = "This is a fake"
+            override fun setCaption(caption: String?): Item = this
+            override fun setValue(format: String?, vararg args: Any?): Item = this
+            override fun setValue(value: Any?): Item = this
+            override fun <T : Any?> setValue(valueProducer: Func<T>?): Item = this
+            override fun <T : Any?> setValue(format: String?, valueProducer: Func<T>?): Item = this
+            override fun setRetained(retained: Boolean?): Item = this
+            override fun isRetained(): Boolean = false
+            override fun addData(caption: String?, format: String?, vararg args: Any?): Item = this
+            override fun addData(caption: String?, value: Any?): Item = this
+            override fun <T : Any?> addData(caption: String?, valueProducer: Func<T>?): Item = this
+            override fun <T : Any?> addData(caption: String?, format: String?, valueProducer: Func<T>?): Item = this
         }
+        private val phoLine = object: Telemetry.Line {
+            override fun addData(caption: String?, format: String?, vararg args: Any?): Item = phoItem
+            override fun addData(caption: String?, value: Any?): Item = phoItem
+            override fun <T : Any?> addData(caption: String?, valueProducer: Func<T>?): Item = phoItem
+            override fun <T : Any?> addData(caption: String?, format: String?, valueProducer: Func<T>?): Item = phoItem
+        }
+        private val phoLog = object: Log {
+            override fun getCapacity(): Int = 0
+            override fun setCapacity(capacity: Int) {
 
-        override fun addData(caption: String?, value: Any?): Telemetry.Item {
-            TODO("Not yet implemented")
-        }
+            }
+            override fun getDisplayOrder(): Log.DisplayOrder = Log.DisplayOrder.NEWEST_FIRST
+            override fun setDisplayOrder(displayOrder: Log.DisplayOrder?) {
 
-        override fun <T : Any?> addData(caption: String?, valueProducer: Func<T>?): Telemetry.Item {
-            TODO("Not yet implemented")
+            }
+            override fun add(entry: String?) {
+
+            }
+            override fun add(format: String?, vararg args: Any?) {
+
+            }
+            override fun clear() {
+
+            }
+
         }
+        private val phoString = "This is a fake"
+
+        override fun addData(caption: String?, format: String?, vararg args: Any?): Item = phoItem
+
+        override fun addData(caption: String?, value: Any?): Item = phoItem
+
+        override fun <T : Any?> addData(caption: String?, valueProducer: Func<T>?): Item = phoItem
 
         override fun <T : Any?> addData(
             caption: String?,
             format: String?,
             valueProducer: Func<T>?
-        ): Telemetry.Item {
-            TODO("Not yet implemented")
-        }
+        ): Item = phoItem
 
-        override fun removeItem(item: Telemetry.Item?): Boolean {
-            TODO("Not yet implemented")
-        }
+        override fun removeItem(item: Telemetry.Item?): Boolean = false
 
         override fun clear() {
-            TODO("Not yet implemented")
+
         }
 
         override fun clearAll() {
-            TODO("Not yet implemented")
+
         }
 
         override fun addAction(action: Runnable?): Any {
-            TODO("Not yet implemented")
+            return phoString
         }
 
-        override fun removeAction(token: Any?): Boolean {
-            TODO("Not yet implemented")
-        }
+        override fun removeAction(token: Any?): Boolean = false
 
         override fun speak(text: String?) {
-            TODO("Not yet implemented")
+
         }
 
         override fun speak(text: String?, languageCode: String?, countryCode: String?) {
-            TODO("Not yet implemented")
+
         }
 
-        override fun update(): Boolean {
-            TODO("Not yet implemented")
-        }
+        override fun update(): Boolean = false
 
-        override fun addLine(): Telemetry.Line {
-            TODO("Not yet implemented")
-        }
+        override fun addLine(): Telemetry.Line = phoLine
 
-        override fun addLine(lineCaption: String?): Telemetry.Line {
-            TODO("Not yet implemented")
-        }
+        override fun addLine(lineCaption: String?): Telemetry.Line = phoLine
 
-        override fun removeLine(line: Telemetry.Line?): Boolean {
-            TODO("Not yet implemented")
-        }
+        override fun removeLine(line: Telemetry.Line?): Boolean = false
 
-        override fun isAutoClear(): Boolean {
-            TODO("Not yet implemented")
-        }
+        override fun isAutoClear(): Boolean = false
 
         override fun setAutoClear(autoClear: Boolean) {
-            TODO("Not yet implemented")
         }
 
-        override fun getMsTransmissionInterval(): Int {
-            TODO("Not yet implemented")
-        }
+        override fun getMsTransmissionInterval(): Int = 0
 
         override fun setMsTransmissionInterval(msTransmissionInterval: Int) {
-            TODO("Not yet implemented")
         }
 
-        override fun getItemSeparator(): String {
-            TODO("Not yet implemented")
-        }
+        override fun getItemSeparator(): String = phoString
 
         override fun setItemSeparator(itemSeparator: String?) {
-            TODO("Not yet implemented")
         }
 
-        override fun getCaptionValueSeparator(): String {
-            TODO("Not yet implemented")
-        }
+        override fun getCaptionValueSeparator(): String = phoString
 
         override fun setCaptionValueSeparator(captionValueSeparator: String?) {
-            TODO("Not yet implemented")
+
         }
 
         override fun setDisplayFormat(displayFormat: Telemetry.DisplayFormat?) {
-            TODO("Not yet implemented")
+
         }
 
-        override fun log(): Telemetry.Log {
-            TODO("Not yet implemented")
-        }
+        override fun log(): Telemetry.Log = phoLog
 
     }
 
