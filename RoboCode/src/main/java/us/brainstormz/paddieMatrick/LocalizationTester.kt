1866355ccb18ea363b52054ce3ca0815f42cb26c
Stu Penrose <stu@penrose.us>
2023-03-04 19:11:55 -0800

    More labtool script stuff

--- RoboCode/src/main/java/us/brainstormz/paddieMatrick/LocalizationTester.kt
+++ RoboCode/src/main/java/us/brainstormz/paddieMatrick/LocalizationTester.kt
@@ -7,15 +7,17 @@ package us.brainstormz.paddieMatrick
 
 //import com.acmerobotics.roadrunner.geometry.Pose2d
 //import com.acmerobotics.roadrunner.trajectory.Trajectory
+import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import us.brainstormz.hardwareClasses.EncoderDriveMovement
+import us.brainstormz.localizer.OdometryFacts
+import us.brainstormz.localizer.OdometryLocalizer
 import us.brainstormz.telemetryWizard.TelemetryConsole
+import java.math.BigDecimal
 
-@Autonomous(name= "Local1zationTe3ter", group= "!")
-//GET THE ENCODER NUMB3RS
-
-class PattieMatrickEncoderTest: OpMode() {
+@Autonomous(name= "LocalizationTester", group= "!")
+class LocalizationTester: OpMode() {
     val hardware = PaddieMatrickHardware()/** Change Depending on robot */
 //    Drivetrain drive = new Drivetrain(hwMap);
     val movement = EncoderDriveMovement(hardware, TelemetryConsole(telemetry))
@@ -34,6 +36,10 @@ class PattieMatrickEncoderTest: OpMode() {
         rOdom = hardware.rightOdomEncoder.currentPosition
         lOdom = hardware.leftOdomEncoder.currentPosition
         cOdom = hardware.centerOdomEncoder.currentPosition
+        val delta = Math.abs(rOdom) - Math.abs(lOdom)
+        telemetry.addData("lr-delta", delta)
+        telemetry.addData("lr-delta rotations",
+            BigDecimal(delta).setScale(5) / BigDecimal(OdometryFacts.ticksPerRotation).setScale(5))
         telemetry.addData("rOdom", rOdom)
         telemetry.addData("lOdom", lOdom)
         telemetry.addData("cOdom", cOdom)
