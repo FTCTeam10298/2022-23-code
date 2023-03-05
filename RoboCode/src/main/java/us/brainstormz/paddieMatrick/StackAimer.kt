2f431445bb46a3fd5e1c8567c49a523308429042
stheyounger <sj2@penrose.us>
2023-03-04 21:59:28 -0800

    now gets angle

--- RoboCode/src/main/java/us/brainstormz/paddieMatrick/StackAimer.kt
+++ RoboCode/src/main/java/us/brainstormz/paddieMatrick/StackAimer.kt
@@ -4,7 +4,6 @@ import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
-import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.openftc.easyopencv.OpenCvCameraRotation
 import us.brainstormz.localizer.PositionAndRotation
@@ -14,23 +13,64 @@ import us.brainstormz.motion.MecanumMovement
 import us.brainstormz.motion.RRLocalizer
 import us.brainstormz.openCvAbstraction.OpenCvAbstraction
 import us.brainstormz.telemetryWizard.TelemetryConsole
-import java.util.Stack
 
 class StackAimer(private val telemetry: Telemetry, private val stackDetector: StackDetector) {
 
-    fun getAngleFromStack(): Double {
-        val centeredPosition: Double = 126.0
-        val stackX = stackDetector.detectedPosition(1000) ?: centeredPosition
-        val errorFromCenter = stackX - centeredPosition
+    fun getStackInchesFromCenter(distanceFromStack: Double): Double {
 
-        telemetry.addLine("centeredPosition: $centeredPosition")
-        telemetry.addLine("stackX: $stackX")
-        telemetry.addLine("errorFromCenter: $errorFromCenter")
+//        val stackInchesFromCenter =
+//
+//        return stackInchesFromCenter
 
-        val turnSpeed = 0.50
-        val angleToStack = (errorFromCenter * turnSpeed).coerceIn(-180.0..180.0)
+        val angleToStack = getAngleToStackRad(distanceFromStack)
+        telemetry.addLine("angleToStack: $angleToStack")
 
-        return angleToStack
+
+        return getPixelsFromCenter()
+    }
+
+    data class Observation(
+        val pixels:Int,
+        val angle:Double,
+        val distance:Int
+    )
+    private val angleByOffset = listOf(
+        Observation(97, 0.0665, 30),// 2" @ 30" distance
+        Observation(83 , 0.0996, 20),// 2" @ 20" distance
+        Observation(78 , 0.13255, 15),// 2" @ 15" distance
+        Observation(74 , 0.197, 10)// 2" @ 10" distance
+    )
+
+    private fun getAngleToStackRad(distanceFromStack: Double): Double? {
+        val pixels = stackDetector.detectedPosition(1000)
+
+        val closestObservation = angleByOffset.minByOrNull {Math.abs(Math.abs(it.distance) - Math.abs(distanceFromStack))}
+
+       return if(pixels!=null && closestObservation!=null){
+           val observationOffset = 120 - closestObservation.pixels
+           val offset = 120 - pixels
+           val angle = (offset.toDouble()/observationOffset.toDouble()) * closestObservation.angle
+
+           return if(pixels > 120){
+               angle
+           }else{
+               angle * -1
+           }
+        }else{
+        null
+       }
+    }
+
+    private fun getPixelsFromCenter(): Double {
+        val centeredOnScreen: Double = 126.0
+        val stackPixels = stackDetector.detectedPosition(1000) ?: centeredOnScreen
+        val pixelsFromCenter = stackPixels - centeredOnScreen
+
+        telemetry.addLine("centeredOnScreen: $centeredOnScreen")
+        telemetry.addLine("stackPixels: $stackPixels")
+        telemetry.addLine("errorFromCenterPixels: $pixelsFromCenter")
+
+        return pixelsFromCenter
     }
 }
 
@@ -55,7 +95,7 @@ class StackAimerTest: OpMode() {
         movement = MecanumMovement(localizer, hardware, telemetry)
 
         opencv.cameraName = "backCam"
-        opencv.cameraOrientation = OpenCvCameraRotation.UPSIDE_DOWN
+        opencv.cameraOrientation = OpenCvCameraRotation.SIDEWAYS_LEFT
         opencv.init(hardwareMap)
         opencv.onNewFrame(stackDetector::processFrame)
         opencv.start()
@@ -63,14 +103,14 @@ class StackAimerTest: OpMode() {
 
     override fun loop() {
 
-        val targetAngle = stackAimer.getAngleFromStack()
-        val position = PositionAndRotation(r= -targetAngle) + movement.localizer.currentPositionAndRotation()
+        val targetAngle = stackAimer.getStackInchesFromCenter(movement.localizer.currentPositionAndRotation().x)
+        val position = PositionAndRotation(x= -targetAngle) + movement.localizer.currentPositionAndRotation()
 
-        val robotIsAtTarget = movement.moveTowardTarget(position, 0.0..1.0)
+//        val robotIsAtTarget = movement.moveTowardTarget(position, 0.0..1.0)
 
         multiTelemetry.addLine("\ntargetAngle: $targetAngle")
         multiTelemetry.addLine("position: $position")
-        multiTelemetry.addLine("\nrobotIsAtTarget: $robotIsAtTarget")
+//        multiTelemetry.addLine("\nrobotIsAtTarget: $robotIsAtTarget")
         multiTelemetry.addLine("current time: ${System.currentTimeMillis()}")
         multiTelemetry.update()
 
