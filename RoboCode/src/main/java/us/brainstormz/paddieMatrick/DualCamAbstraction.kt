0f8d38b014fda4a1593f461efe46c8e95ce62c9e
stheyounger <sj2@penrose.us>
2023-03-09 15:45:23 -0800

    moved multicam test to it's own folder

--- RoboCode/src/main/java/us/brainstormz/paddieMatrick/DualCamAbstraction.kt
+++ RoboCode/src/main/java/us/brainstormz/paddieMatrick/DualCamAbstraction.kt
@@ -1,17 +1,9 @@
 package us.brainstormz.paddieMatrick
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
 import org.openftc.easyopencv.*
 import org.openftc.easyopencv.OpenCvCamera.AsyncCameraOpenListener
-import us.brainstormz.localizer.PhoHardware
-import us.brainstormz.localizer.StackDetector
-import us.brainstormz.localizer.StackDetectorVars
-import us.brainstormz.localizer.polePosition.creamsicleGoalDetection.CreamsicleGoalDetector
-import us.brainstormz.openCvAbstraction.PipelineAbstraction
-import us.brainstormz.telemetryWizard.TelemetryConsole
 
 class DualCamAbstraction(private val hardwareMap: HardwareMap) {
 
@@ -33,7 +25,6 @@ class DualCamAbstraction(private val hardwareMap: HardwareMap) {
         newCamera.showFpsMeterOnViewport(false)
 
         newCamera.openCameraDeviceAsync(object : AsyncCameraOpenListener {
-
             override fun onOpened() {
                 println("[camera/$cameraName] opened ")
                 newCamera.setPipeline(pipeline)
@@ -50,54 +41,3 @@ class DualCamAbstraction(private val hardwareMap: HardwareMap) {
 }
 
 
-@Autonomous
-class MulticamTest : LinearOpMode() {
-
-    override fun runOpMode() {
-
-        val dualCamAbstraction = DualCamAbstraction(hardwareMap)
-        val viewportContainerIds = dualCamAbstraction.setupViewport()
-
-
-        val liftCamName = "liftCam"
-
-        val junctionDetector = CreamsicleGoalDetector(TelemetryConsole(PhoHardware.PhoTelemetry()))
-        val liftPipeline = PipelineAbstraction()
-        liftPipeline.userFun = junctionDetector::scoopFrame
-
-        val liftCam: OpenCvCamera = dualCamAbstraction.startNewCamera(
-            cameraName = liftCamName,
-            cameraRotation = OpenCvCameraRotation.UPSIDE_DOWN,
-            viewportContainerId = viewportContainerIds[0],
-            pipeline = liftPipeline)
-
-
-        val backCamName = "backCam"
-
-        val stackDetectorVars = StackDetectorVars(StackDetector.TargetHue.BLUE, StackDetector.Mode.FRAME)
-        val stackDetector = StackDetector(stackDetectorVars, telemetry)
-        val backPipeline = PipelineAbstraction()
-        backPipeline.userFun = stackDetector::processFrame
-
-        val backCam: OpenCvCamera = dualCamAbstraction.startNewCamera(
-            cameraName = backCamName,
-            cameraRotation = OpenCvCameraRotation.UPRIGHT,
-            viewportContainerId = viewportContainerIds[1],
-            pipeline = backPipeline)
-
-        waitForStart()
-
-
-        while (opModeIsActive()) {
-            telemetry.addLine("$liftCamName FPS: ${liftCam.fps}")
-            telemetry.addLine("$backCamName FPS: ${backCam.fps}")
-            telemetry.update()
-            sleep(100)
-            if (isStopRequested) {
-                liftCam.closeCameraDeviceAsync { liftCam.closeCameraDevice() }
-                backCam.closeCameraDeviceAsync { backCam.closeCameraDevice() }
-            }
-        }
-    }
-
-}
---
+++ RoboCode/src/main/java/us/brainstormz/paddieMatrick/MulticamTest.kt
@@ -0,0 +1,65 @@
+package us.brainstormz.paddieMatrick
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.openftc.easyopencv.OpenCvCamera
+import org.openftc.easyopencv.OpenCvCameraRotation
+import us.brainstormz.localizer.PhoHardware
+import us.brainstormz.localizer.StackDetector
+import us.brainstormz.localizer.StackDetectorVars
+import us.brainstormz.localizer.polePosition.creamsicleGoalDetection.CreamsicleGoalDetector
+import us.brainstormz.openCvAbstraction.PipelineAbstraction
+import us.brainstormz.telemetryWizard.TelemetryConsole
+
+@Autonomous
+class MulticamTest : LinearOpMode() {
+
+    override fun runOpMode() {
+
+        val dualCamAbstraction = DualCamAbstraction(hardwareMap)
+        val viewportContainerIds = dualCamAbstraction.setupViewport()
+
+
+        val liftCamName = "liftCam"
+
+        val junctionDetector = CreamsicleGoalDetector(TelemetryConsole(PhoHardware.PhoTelemetry()))
+        val liftPipeline = PipelineAbstraction()
+        liftPipeline.userFun = junctionDetector::scoopFrame
+
+        val liftCam: OpenCvCamera = dualCamAbstraction.startNewCamera(
+            cameraName = liftCamName,
+            cameraRotation = OpenCvCameraRotation.UPSIDE_DOWN,
+            viewportContainerId = viewportContainerIds[0],
+            pipeline = liftPipeline)
+
+
+        val backCamName = "backCam"
+
+        val stackDetectorVars =
+            StackDetectorVars(StackDetector.TargetHue.BLUE, StackDetector.Mode.FRAME)
+        val stackDetector = StackDetector(stackDetectorVars, telemetry)
+        val backPipeline = PipelineAbstraction()
+        backPipeline.userFun = stackDetector::processFrame
+
+        val backCam: OpenCvCamera = dualCamAbstraction.startNewCamera(
+            cameraName = backCamName,
+            cameraRotation = OpenCvCameraRotation.UPSIDE_DOWN,
+            viewportContainerId = viewportContainerIds[1],
+            pipeline = backPipeline)
+
+        waitForStart()
+
+
+        while (opModeIsActive()) {
+            telemetry.addLine("$liftCamName FPS: ${liftCam.fps}")
+            telemetry.addLine("$backCamName FPS: ${backCam.fps}")
+            telemetry.update()
+            sleep(100)
+            if (isStopRequested) {
+                liftCam.closeCameraDeviceAsync { liftCam.closeCameraDevice() }
+                backCam.closeCameraDeviceAsync { backCam.closeCameraDevice() }
+            }
+        }
+    }
+
+}
