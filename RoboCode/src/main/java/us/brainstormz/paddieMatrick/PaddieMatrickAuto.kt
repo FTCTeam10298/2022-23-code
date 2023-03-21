2b75305cf2c69c67ac2f82d1c17a2c1acbfd859e
stheyounger <sj2@penrose.us>
2023-03-20 18:49:48 -0700

    Revert "two tape linups"
    This reverts commit b356e9d9c0ee9a6584bc9d830b741568618f504b.
    
    # Conflicts:
    #	RoboCode/src/main/java/us/brainstormz/paddieMatrick/PaddieMatrickAuto.kt

--- RoboCode/src/main/java/us/brainstormz/paddieMatrick/PaddieMatrickAuto.kt
+++ RoboCode/src/main/java/us/brainstormz/paddieMatrick/PaddieMatrickAuto.kt
@@ -159,34 +159,18 @@ class PaddieMatrickAuto: OpMode() {
                 timeoutSeconds = 2.0
             ),
             AutoTask(
-                ChassisTask(
-                    collectionApproachPosition,
-                    power= 0.0..0.8,
-                    accuracyInches= 0.0,
-                    xTranslationPID = MecanumMovement.fineMoveXTranslation,
-                    requiredForCompletion = false),
-                LiftTask(Depositor.LiftCounts.StackPreCollection.counts, requiredForCompletion = true),
-                FourBarTask(Depositor.FourBarDegrees.StackCollecting.degrees, accuracyDegrees = 6.0, requiredForCompletion = true),
-                OtherTask(isDone = { false }, requiredForCompletion = true),
-                nextTaskIteration = {
-                    tapeLineupNeseccary(it, withPrelinupCorrection(it))
-                },
-                timeoutSeconds = 3.0
-            ),
-            AutoTask(
-                ChassisTask(
-                    collectionApproachPosition,
-                    power= 0.0..0.8,
-                    accuracyInches= 0.0,
-                    xTranslationPID = MecanumMovement.fineMoveXTranslation,
-                    requiredForCompletion = false),
-                LiftTask(Depositor.LiftCounts.StackPreCollection.counts, requiredForCompletion = true),
-                FourBarTask(Depositor.FourBarDegrees.StackCollecting.degrees, accuracyDegrees = 6.0, requiredForCompletion = true),
-                OtherTask(isDone = { false }, requiredForCompletion = true),
-                nextTaskIteration = {
-                    tapeLineupNeseccary(it, withPrelinupCorrection(it))
-                },
-                timeoutSeconds = 3.0
+                    ChassisTask(
+                        collectionApproachPosition,
+                        accuracyInches= 0.0,
+                        xTranslationPID = MecanumMovement.fineMoveXTranslation,
+                        requiredForCompletion = false),
+                    LiftTask(Depositor.LiftCounts.StackPreCollection.counts, requiredForCompletion = true),
+                    FourBarTask(Depositor.FourBarDegrees.StackCollecting.degrees, accuracyDegrees = 6.0, requiredForCompletion = true),
+                    OtherTask(isDone = { false }, requiredForCompletion = true),
+                    nextTaskIteration = {
+                        tapeLineupNeseccary(it, withPrelinupCorrection(it))
+                    },
+                    timeoutSeconds = 3.0
             ),
     )
 
@@ -330,7 +314,6 @@ class PaddieMatrickAuto: OpMode() {
 
         var newTargetPos = PositionAndRotation()
         val completionTask = if (changedWhatWeSee) {
-            colorFirstSeen = null
             newTargetPos = movement.localizer.currentPositionAndRotation()
             OtherTask(isDone= {true}, requiredForCompletion = false)
         } else {
